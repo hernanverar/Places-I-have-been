15b4fce992564ddd21fe5ffdbfcb9c04c070cf07
hernan verar <hernanverar@gmail.com>
2023-04-03 15:38:30 -0700

    crearted a list of citys in my js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,15 @@ function PlacesToVisit () {
 
 PlacesToVisit.prototype.addCity = function (city) {
   this.citys[city.cityName] = city;
-}
+};
+
+PlacesToVisit.prototype.findCity = function (id) {
+  if (this.citys[id] !== undefined) {
+  return this.citys[id];
+  }
+  return false;
+};
+
 
 function City (cityName, cityThigsToDo,) {
   this.cityName = cityName;
@@ -14,3 +22,18 @@ function City (cityName, cityThigsToDo,) {
 City.prototype.cityName = function() {
   return this.cityName + " " + this.cityThingsToDo;
 };
+
+function listCitys(placesToVisitToDisplay) {
+  let citysDiv = document.querySelector("div#cityName");
+  citysDiv.innertext = null;
+
+  const ul = document.createElement('ul');
+  Object.keys(placesToVisitToDisplay.citys).forEach(function(key){
+    const city = placesToVisitToDisplay.findCity(key);
+    const li = document.creteElement("li");
+    li.append(city.cityName());
+    li,setAttribute("id", city.id);
+    ul.append(li);
+  });
+  citysDiv.append(ul);
+}
